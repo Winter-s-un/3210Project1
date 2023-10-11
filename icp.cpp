a5c6572407c62bd4a0b562cc1dd394b97fed2c6b
Winter-s-un <114461028+Winter-s-un@users.noreply.github.com>
2023-10-11 18:03:44 +0800

    Update icp.cpp

--- icp.cpp
+++ icp.cpp
@@ -27,7 +27,6 @@ void findCorrespondences(pcl::PointCloud<pcl::PointXYZ>::Ptr src_cloud, pcl::Poi
     }
 }
 
-
 // 使用Eigen库的Matrix4d定义变换矩阵
 typedef Eigen::Matrix4d TransformationMatrix;
 
@@ -50,22 +49,16 @@ std::vector<Eigen::Vector3d> computeMeanRemovedPoints(const pcl::PointCloud<pcl:
     return mean_removed_points;
 }
 
-Eigen::Vector3d computeTranslation(const pcl::PointCloud<pcl::PointXYZ>::Ptr src_cloud, const pcl::PointCloud<pcl::PointXYZ>::Ptr tar_cloud, const std::vector<int>& correspondences) {
-    std::vector<Eigen::Vector3d> mean_removed_points = computeMeanRemovedPoints(src_cloud, tar_cloud, correspondences);
-
-    Eigen::Vector3d translation_vector = Eigen::Vector3d::Zero();
-    for (const Eigen::Vector3d& point : mean_removed_points) {
-        translation_vector += point;
-    }
-    translation_vector /= mean_removed_points.size();
-
-    return translation_vector;
-}
-
-
+// 使用SVD计算变换矩阵
 TransformationMatrix computeTransformation(const pcl::PointCloud<pcl::PointXYZ>::Ptr src_cloud, const pcl::PointCloud<pcl::PointXYZ>::Ptr tar_cloud, const std::vector<int>& correspondences) {
     // 步骤1：计算均值移除后的点对列表
     std::vector<Eigen::Vector3d> mean_removed_points = computeMeanRemovedPoints(src_cloud, tar_cloud, correspondences);
+
+    // Debug输出：均值移除后的点对列表
+    std::cout << "Mean Removed Points:" << std::endl;
+    for (const Eigen::Vector3d& point : mean_removed_points) {
+        std::cout << point.transpose() << std::endl;
+    }
 
     // 步骤2：计算协方差矩阵
     Eigen::Matrix3d covariance_matrix = Eigen::Matrix3d::Zero();
@@ -74,29 +67,36 @@ TransformationMatrix computeTransformation(const pcl::PointCloud<pcl::PointXYZ>:
     }
     covariance_matrix /= mean_removed_points.size();
 
+    // Debug输出：协方差矩阵
+    std::cout << "Covariance Matrix:" << std::endl;
+    std::cout << covariance_matrix << std::endl;
+
     // 步骤3：SVD分解协方差矩阵
     Eigen::JacobiSVD<Eigen::Matrix3d> svd(covariance_matrix, Eigen::ComputeFullU | Eigen::ComputeFullV);
     Eigen::Matrix3d rotation_matrix = svd.matrixU() * svd.matrixV().transpose();
 
+    // Debug输出：旋转矩阵
+    std::cout << "Rotation Matrix:" << std::endl;
+    std::cout << rotation_matrix << std::endl;
+
     // 步骤4：构建变换矩阵
     TransformationMatrix transformation = TransformationMatrix::Identity();
     transformation.block(0, 0, 3, 3) = rotation_matrix;
-    
-    // 步骤4（续）：添加平移部分到变换矩阵
-    Eigen::Vector3d translation_vector = computeTranslation(src_cloud, tar_cloud, correspondences);
-    transformation.block(0, 3, 3, 1) = translation_vector;
+
+    // Debug输出：变换矩阵
+    std::cout << "Transformation Matrix:" << std::endl;
+    std::cout << transformation << std::endl;
 
     return transformation;
 }
 
-
-
 Eigen::Matrix4d icp_registration(pcl::PointCloud<pcl::PointXYZ>::Ptr src_cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr tar_cloud, Eigen::Matrix4d init_guess) {
-    Eigen::Matrix4d transformation = Eigen::Matrix4d::Identity();
     int iteration = 0;
 
     KDTree kdtree;
     kdtree.setInputCloud(tar_cloud);
+
+    Eigen::Matrix4d transformation = init_guess;
 
     while (iteration < params::max_iterations) {
         std::vector<int> correspondences;
@@ -119,7 +119,10 @@ Eigen::Matrix4d icp_registration(pcl::PointCloud<pcl::PointXYZ>::Ptr src_cloud, 
         iteration++;
     }
 
+    // Debug输出：ICP结果的变换矩阵
+    std::cout << "Final Transformation:" << std::endl;
+    std::cout << transformation << std::endl;
+
     return transformation;
 }
-
 
