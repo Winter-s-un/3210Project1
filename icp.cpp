ac3ead8ecc67e20e65b69605427352bc38a09fcb
Winter-s-un <114461028+Winter-s-un@users.noreply.github.com>
2023-10-11 18:39:57 +0800

    Update icp.cpp

--- icp.cpp
+++ icp.cpp
@@ -24,55 +24,54 @@ void findCorrespondences(pcl::PointCloud<pcl::PointXYZ>::Ptr src_cloud, pcl::Poi
 
 typedef Eigen::Matrix4d TransformationMatrix;
 
-// 计算均值移除后的点对列表
-std::vector<Eigen::Vector3d> computeMeanRemovedPoints(const pcl::PointCloud<pcl::PointXYZ>::Ptr src_cloud, const pcl::PointCloud<pcl::PointXYZ>::Ptr tar_cloud, const std::vector<int>& correspondences) {
-    std::vector<Eigen::Vector3d> mean_removed_points;
+TransformationMatrix computeTransformation(const pcl::PointCloud<PointType>::Ptr src_cloud, const pcl::PointCloud<PointType>::Ptr tar_cloud, const std::vector<int>& correspondences) {
+    // Compute the centroid of the source cloud
+    Eigen::Vector3d src_centroid(0, 0, 0);
+    for (size_t i = 0; i < src_cloud->size(); ++i) {
+        src_centroid.x() += src_cloud->points[i].x;
+        src_centroid.y() += src_cloud->points[i].y;
+        src_centroid.z() += src_cloud->points[i].z;
+    }
+    src_centroid /= src_cloud->size();
 
+    // Compute the centroid of the target cloud
+    Eigen::Vector3d tar_centroid(0, 0, 0);
     for (size_t i = 0; i < correspondences.size(); ++i) {
-        pcl::PointXYZ src_point = src_cloud->points[i];
-        pcl::PointXYZ tar_point = tar_cloud->points[correspondences[i]];
-
-        Eigen::Vector3d mean_removed_point;
-        mean_removed_point(0) = src_point.x - tar_point.x;
-        mean_removed_point(1) = src_point.y - tar_point.y;
-        mean_removed_point(2) = src_point.z - tar_point.z;
-
-        mean_removed_points.push_back(mean_removed_point);
+        tar_centroid.x() += tar_cloud->points[correspondences[i]].x;
+        tar_centroid.y() += tar_cloud->points[correspondences[i]].y;
+        tar_centroid.z() += tar_cloud->points[correspondences[i]].z;
     }
+    tar_centroid /= correspondences.size();
 
-    return mean_removed_points;
-}
-
-TransformationMatrix computeTransformation(const pcl::PointCloud<pcl::PointXYZ>::Ptr src_cloud, const pcl::PointCloud<pcl::PointXYZ>::Ptr tar_cloud, const std::vector<int>& correspondences) {
-    // 步骤1：计算均值移除后的点对列表
-    std::vector<Eigen::Vector3d> mean_removed_points = computeMeanRemovedPoints(src_cloud, tar_cloud, correspondences);
-
-    // 步骤2：计算均值偏移
-    Eigen::Vector3d mean_offset = Eigen::Vector3d::Zero();
-    for (const Eigen::Vector3d& point : mean_removed_points) {
-        mean_offset += point;
-    }
-    mean_offset /= mean_removed_points.size();
-
-    // 步骤3：计算协方差矩阵
+    // Compute the covariance matrix
     Eigen::Matrix3d covariance_matrix = Eigen::Matrix3d::Zero();
-    for (const Eigen::Vector3d& point : mean_removed_points) {
-        Eigen::Vector3d centered_point = point - mean_offset;
-        covariance_matrix += centered_point * centered_point.transpose();
-    }
-    covariance_matrix /= mean_removed_points.size();
+    for (size_t i = 0; i < correspondences.size(); ++i) {
+        Eigen::Vector3d src_point, tar_point;
+        src_point.x() = src_cloud->points[i].x - src_centroid.x();
+        src_point.y() = src_cloud->points[i].y - src_centroid.y();
+        src_point.z() = src_cloud->points[i].z - src_centroid.z();
+        tar_point.x() = tar_cloud->points[correspondences[i]].x - tar_centroid.x();
+        tar_point.y() = tar_cloud->points[correspondences[i]].y - tar_centroid.y();
+        tar_point.z() = tar_cloud->points[correspondences[i]].z - tar_centroid.z();
 
-    // 步骤4：SVD分解协方差矩阵
+        covariance_matrix += src_point * tar_point.transpose();
+    }
+    covariance_matrix /= correspondences.size();
+
+    // Compute the Singular Value Decomposition (SVD) to find rotation
     Eigen::JacobiSVD<Eigen::Matrix3d> svd(covariance_matrix, Eigen::ComputeFullU | Eigen::ComputeFullV);
     Eigen::Matrix3d rotation_matrix = svd.matrixU() * svd.matrixV().transpose();
 
-    // 步骤5：构建变换矩阵
+    // Compute the translation vector
+    Eigen::Vector3d translation = tar_centroid - rotation_matrix * src_centroid;
+
+    // Construct the transformation matrix
     TransformationMatrix transformation = TransformationMatrix::Identity();
-    transformation.block<3, 3>(0, 0) = rotation_matrix;
-    transformation.block<3, 1>(0, 3) = mean_offset;
+    transformation.block(0, 0, 3, 3) = rotation_matrix;
+    transformation.block(0, 3, 3, 1) = translation;
+
     return transformation;
 }
-
 
 Eigen::Matrix4d icp_registration(pcl::PointCloud<pcl::PointXYZ>::Ptr src_cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr tar_cloud, Eigen::Matrix4d init_guess) {
     int iteration = 0;
