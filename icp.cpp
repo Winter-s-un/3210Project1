4351e419164adbb37a9435062efb7703b392e439
Winter-s-un <114461028+Winter-s-un@users.noreply.github.com>
2023-10-11 17:36:16 +0800

    Add files via upload

--- icp.cpp
+++ icp.cpp
@@ -5,121 +5,29 @@
 **/
 #include "icp.h"
 #include <pcl/registration/icp.h>
-#include <pcl/kdtree/kdtree_flann.h> //include Kdtree header
-#include "parameters.h"
-#include <vector>
-#include <cmath>
-
-
-
-typedef pcl::KdTreeFLANN<pcl::PointXYZ> KDTree;
-
-void findCorrespondences(pcl::PointCloud<pcl::PointXYZ>::Ptr src_cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr tar_cloud, std::vector<int>& correspondences, KDTree& kdtree) {
-    for (const pcl::PointXYZ& src_point : *src_cloud) {
-        std::vector<int> indices(1);
-        std::vector<float> distances(1);
-
-        // 使用KD树查找最近邻点的索引
-        kdtree.nearestKSearch(src_point, 1, indices, distances);
-
-        // 添加最近邻点的索引到correspondences数组
-        correspondences.push_back(indices[0]);
-    }
-}
-
-
-// 使用Eigen库的Matrix4d定义变换矩阵
-typedef Eigen::Matrix4d TransformationMatrix;
-
-// 计算均值移除后的点对列表
-std::vector<Eigen::Vector3d> computeMeanRemovedPoints(const pcl::PointCloud<pcl::PointXYZ>::Ptr src_cloud, const pcl::PointCloud<pcl::PointXYZ>::Ptr tar_cloud, const std::vector<int>& correspondences) {
-    std::vector<Eigen::Vector3d> mean_removed_points;
-
-    for (size_t i = 0; i < correspondences.size(); ++i) {
-        pcl::PointXYZ src_point = src_cloud->points[i];
-        pcl::PointXYZ tar_point = tar_cloud->points[correspondences[i]];
-
-        Eigen::Vector3d mean_removed_point;
-        mean_removed_point(0) = src_point.x - tar_point.x;
-        mean_removed_point(1) = src_point.y - tar_point.y;
-        mean_removed_point(2) = src_point.z - tar_point.z;
-
-        mean_removed_points.push_back(mean_removed_point);
-    }
-
-    return mean_removed_points;
-}
-
-Eigen::Vector3d computeTranslation(const pcl::PointCloud<pcl::PointXYZ>::Ptr src_cloud, const pcl::PointCloud<pcl::PointXYZ>::Ptr tar_cloud, const std::vector<int>& correspondences) {
-    std::vector<Eigen::Vector3d> mean_removed_points = computeMeanRemovedPoints(src_cloud, tar_cloud, correspondences);
-
-    Eigen::Vector3d translation_vector = Eigen::Vector3d::Zero();
-    for (const Eigen::Vector3d& point : mean_removed_points) {
-        translation_vector += point;
-    }
-    translation_vector /= mean_removed_points.size();
-
-    return translation_vector;
-}
-
-
-TransformationMatrix computeTransformation(const pcl::PointCloud<pcl::PointXYZ>::Ptr src_cloud, const pcl::PointCloud<pcl::PointXYZ>::Ptr tar_cloud, const std::vector<int>& correspondences) {
-    // 步骤1：计算均值移除后的点对列表
-    std::vector<Eigen::Vector3d> mean_removed_points = computeMeanRemovedPoints(src_cloud, tar_cloud, correspondences);
-
-    // 步骤2：计算协方差矩阵
-    Eigen::Matrix3d covariance_matrix = Eigen::Matrix3d::Zero();
-    for (const Eigen::Vector3d& point : mean_removed_points) {
-        covariance_matrix += point * point.transpose();
-    }
-    covariance_matrix /= mean_removed_points.size();
-
-    // 步骤3：SVD分解协方差矩阵
-    Eigen::JacobiSVD<Eigen::Matrix3d> svd(covariance_matrix, Eigen::ComputeFullU | Eigen::ComputeFullV);
-    Eigen::Matrix3d rotation_matrix = svd.matrixU() * svd.matrixV().transpose();
-
-    // 步骤4：构建变换矩阵
-    TransformationMatrix transformation = TransformationMatrix::Identity();
-    transformation.block(0, 0, 3, 3) = rotation_matrix;
-    
-    // 步骤4（续）：添加平移部分到变换矩阵
-    Eigen::Vector3d translation_vector = computeTranslation(src_cloud, tar_cloud, correspondences);
-    transformation.block(0, 3, 3, 1) = translation_vector;
-
-    return transformation;
-}
-
-
 
 Eigen::Matrix4d icp_registration(pcl::PointCloud<pcl::PointXYZ>::Ptr src_cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr tar_cloud, Eigen::Matrix4d init_guess) {
-    Eigen::Matrix4d transformation = Eigen::Matrix4d::Identity();
-    int iteration = 0;
+  // This is an example of using pcl::IterativeClosestPoint to align two point clouds
+  // In your project, you should implement your own ICP algorithm!!!
+  // In your implementation, you can use KDTree in PCL library to find nearest neighbors
+  // Use chatGPT, google and github to learn how to use PCL library and implement ICP. But do not copy totally. TA will check your code using advanced tools.
+  // If you use other's code, you should add a reference in your report. https://registry.hkust.edu.hk/resource-library/academic-integrity
 
-    KDTree kdtree;
-    kdtree.setInputCloud(tar_cloud);
+  pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+  icp.setInputSource(src_cloud);
+  icp.setInputTarget(tar_cloud);
+  icp.setMaximumIterations(params::max_iterations);  // set maximum iteration
+  icp.setTransformationEpsilon(1e-6);  // set transformation epsilon
+  icp.setMaxCorrespondenceDistance(params::max_distance);  // set maximum correspondence distance
+  pcl::PointCloud<pcl::PointXYZ> aligned_cloud;
+  icp.align(aligned_cloud, init_guess.cast<float>());
 
-    while (iteration < params::max_iterations) {
-        std::vector<int> correspondences;
-        findCorrespondences(src_cloud, tar_cloud, correspondences, kdtree);
-
-        // 计算变换矩阵
-        Eigen::Matrix4d delta_transformation = computeTransformation(src_cloud, tar_cloud, correspondences);
-
-        // 更新变换矩阵
-        transformation = delta_transformation * transformation;
-
-        // 计算变换矩阵的变化量
-        double transformation_change = (delta_transformation - Eigen::Matrix4d::Identity()).norm();
-
-        // 收敛检查：如果变换矩阵的变化量小于收敛阈值，退出迭代
-        if (transformation_change < 1e-6) {
-            break;
-        }
-
-        iteration++;
-    }
-
-    return transformation;
+  Eigen::Matrix4d transformation = icp.getFinalTransformation().cast<double>();
+  return transformation;
 }
 
-
+void update_local_map(pcl::PointCloud<pcl::PointXYZ>::Ptr local_map, pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_scan) {
+  // Update the local map by adding the transformed scan.
+  pcl::transformPointCloud(*transformed_scan, *transformed_scan, Twb.cast<float>());
+  *local_map += *transformed_scan;
+}
